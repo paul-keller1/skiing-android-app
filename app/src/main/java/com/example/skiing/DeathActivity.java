c39f4b4e99a176f29c8d1d2e6db1c2247eda903d
paulkeller <paul.keller@online.de>
2025-10-20 11:18:41 +0200

    setup

---
+++ app/src/main/java/com/example/skiing/DeathActivity.java
@@ -0,0 +1,10 @@
+package com.example.skiing;
+
+import android.app.Activity;
+
+public class DeathActivity extends Activity {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/skiing/MenuActivity.java
@@ -0,0 +1,4 @@
+package com.example.skiing;
+
+public class MenuActivity {
+}
